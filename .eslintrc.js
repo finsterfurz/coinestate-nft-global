399937d48111648c8e909fde2cae3529b4d5fb41
denis <139350389+finsterfurz@users.noreply.github.com>
2025-06-20 01:51:46 +0200

    Add comprehensive ESLint configuration for React and Web3 development

---
+++ .eslintrc.js
@@ -0,0 +1,140 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+  ],
+  rules: {
+    // React specific rules
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/prop-types': 'warn',
+    'react/display-name': 'warn',
+    'react/jsx-key': 'error',
+    'react/jsx-no-duplicate-props': 'error',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/no-children-prop': 'error',
+    'react/no-danger-with-children': 'error',
+    'react/no-deprecated': 'warn',
+    'react/no-direct-mutation-state': 'error',
+    'react/no-find-dom-node': 'error',
+    'react/no-is-mounted': 'error',
+    'react/no-render-return-value': 'error',
+    'react/no-string-refs': 'error',
+    'react/no-unescaped-entities': 'error',
+    'react/no-unknown-property': 'error',
+    'react/require-render-return': 'error',
+    'react/self-closing-comp': 'warn',
+
+    // React Hooks rules
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // Accessibility rules
+    'jsx-a11y/alt-text': 'warn',
+    'jsx-a11y/anchor-has-content': 'warn',
+    'jsx-a11y/anchor-is-valid': 'warn',
+    'jsx-a11y/aria-activedescendant-has-tabindex': 'warn',
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-role': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/heading-has-content': 'warn',
+    'jsx-a11y/iframe-has-title': 'warn',
+    'jsx-a11y/img-redundant-alt': 'warn',
+    'jsx-a11y/no-access-key': 'warn',
+    'jsx-a11y/no-distracting-elements': 'warn',
+    'jsx-a11y/no-redundant-roles': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'jsx-a11y/scope': 'warn',
+
+    // General JavaScript rules
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'no-unused-vars': ['warn', { 
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_',
+      ignoreRestSiblings: true 
+    }],
+    'no-undef': 'error',
+    'no-unreachable': 'error',
+    'no-duplicate-imports': 'error',
+    'prefer-const': 'warn',
+    'no-var': 'error',
+    'eqeqeq': ['error', 'smart'],
+    'curly': ['warn', 'multi-line'],
+    'brace-style': ['warn', '1tbs', { allowSingleLine: true }],
+    'comma-dangle': ['warn', 'only-multiline'],
+    'quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'semi': ['warn', 'always'],
+    'indent': ['warn', 2, { SwitchCase: 1 }],
+    'max-len': ['warn', { 
+      code: 120, 
+      ignoreUrls: true, 
+      ignoreStrings: true, 
+      ignoreTemplateLiterals: true 
+    }],
+
+    // Security rules for blockchain/Web3 development
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+    'no-new-func': 'error',
+    'no-script-url': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.test.js', '**/*.test.jsx', '**/*.spec.js', '**/*.spec.jsx'],
+      env: {
+        jest: true,
+      },
+      rules: {
+        'no-console': 'off',
+      },
+    },
+    {
+      files: ['src/setupTests.js'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
+  ignorePatterns: [
+    'build/',
+    'dist/',
+    'node_modules/',
+    'coverage/',
+    '*.min.js',
+    'public/static/',
+  ],
+};
